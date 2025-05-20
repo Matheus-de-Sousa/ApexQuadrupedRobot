1657803bf2c08d6005cbaa473cefdccec87af2ba
Matheus de Sousa <matheus.sousa3089@gmail.com>
2025-05-20 01:21:39 -0300

    acionamento servomotores

--- scr/Robot/Leg.py
+++ scr/Robot/Leg.py
@@ -1,12 +1,25 @@
+from adafruit_servokit import ServoKit
 class Leg:
+    Joints = ServoKit(channels=16)
     FemurLength = 0
     TibiaLength = 0
     SholderOffset = 0
 
-    def __init__(self, femurlength, tibialength, shoulderoffset):
+    # Canais dos sevos das juntas no driver
+    footJointChannel = 0
+    legJointChannel = 0
+    shoulderJointChannel = 0
+
+    def __init__(self, femurlength, tibialength, shoulderoffset, JointsChannels):
         self.FemurLength = femurlength
         self.TibiaLength = tibialength
         self.SholderOffset = shoulderoffset
+
+        self.footJointChannel = JointsChannels[0]
+        self.legJointChannel = JointsChannels[1]
+        self.shoulderJointChannel = JointsChannels[2]
     
     def setJointAngle(self, foot, leg, shoulder):
-        #Acionamento do servo
+        self.Joints.servo[self.footJointChannel].angle = foot
+        self.Joints.servo[self.legJointChannel].angle = leg
+        self.Joints.servo[self.shoulderJointChannel].angle = shoulder
